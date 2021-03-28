371048a4e7092fd7d8f1762552136ecd4ceefb6f
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-29 01:51:03 +0900

    Setters and getters have been implemented

--- src/Assignment/Practice1.java
+++ src/Assignment/Practice1.java
@@ -1,3 +1,5 @@
+package Assignment;
+
 public class Practice1 {
     public static void main(String[] args){
         System.out.println("Programming is fun!");
--- src/Assignment/Practice6.java
+++ src/Assignment/Practice6.java
@@ -1,4 +1,4 @@
-
+package Assignment;
 
 public class Practice6 {
     public static void main(String[] args){
