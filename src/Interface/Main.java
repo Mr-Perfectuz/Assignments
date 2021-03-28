b752c602791ed6176e7ae9eaf2ad91848efc6b56
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-29 05:53:35 +0900

    interface has been implemented

---
+++ src/Interface/Main.java
@@ -0,0 +1,11 @@
+package Interface;
+
+public class Main {
+    public static void main(String[] args){
+//        Interface = a template that can be applied to a class
+//        similar to inheritance, but specifies what a class has/must do
+//        classes can apply more than one interface, inheritance is limited to one super class
+
+    }
+
+}
