24a884ae129d94ecc7f901580330a8012e2e7d34
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-29 13:59:02 +0900

    Polymorphism has been implemented

---
+++ src/Polymorphism/Boat.java
@@ -0,0 +1,4 @@
+package Polymorphism;
+
+public class Boat extends Vehicle{
+}
--- src/Polymorphism/Main.java
+++ src/Polymorphism/Main.java
@@ -3,5 +3,8 @@ package Polymorphism;
 public class Main {
     public static void main(String[] args){
 
+//        polymorphism  = greek word for poly = "many" , morph = "form"
+//        The ability of an object to identify as more than one type
+
     }
 }
