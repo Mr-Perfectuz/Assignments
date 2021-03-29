f22ef85b5d4c4d75c57a0cf1a25563e03b4e4773
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-29 13:51:03 +0900

    interface has been implemented

--- src/Interface/Fish.java
+++ src/Interface/Fish.java
@@ -1,4 +1,14 @@
 package Interface;
 
-public class Fish {
+public class Fish implements Prey,Predetor{
+    @Override
+    public void hunt() {
+        System.out.println("This fish is hunting for a smaller fish");
+    }
+
+    @Override
+    public void flee() {
+        System.out.println("This fish fleeing from a larger fish");
+
+    }
 }
--- src/Interface/Main.java
+++ src/Interface/Main.java
@@ -6,10 +6,13 @@ public class Main {
 //        similar to inheritance, but specifies what a class has/must do
 //        classes can apply more than one interface, inheritance is limited to one super class
 
-        Rabbit rabbit = new Rabbit();
-        rabbit.flee();
-        Hawk hawk = new Hawk();
-        hawk.hunt();
+//        Rabbit rabbit = new Rabbit();
+//        rabbit.flee();
+//        Hawk hawk = new Hawk();
+//        hawk.hunt();
+        Fish fish = new Fish();
+        fish.flee();
+        fish.hunt();
 
     }
 
