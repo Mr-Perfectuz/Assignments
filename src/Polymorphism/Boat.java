b1eeb952e952e073c00a267e7567b89a00271386
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-29 14:25:39 +0900

    Polymorphism has been implemented

--- src/Polymorphism/Boat.java
+++ src/Polymorphism/Boat.java
@@ -1,4 +1,9 @@
 package Polymorphism;
 
 public class Boat extends Vehicle{
+    Car car = new Car();
+    Bicycle bicycle = new Bicycle();
+    Boat boat = new Boat();
+
+    racers = {car, bicycle, boat};
 }
