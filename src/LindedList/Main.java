581ee18e81733c5a79b83d836255afbe3f26148a
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-30 23:19:00 +0900

    Polymorphism has been implemented

--- src/LindedList/Main.java
+++ src/LindedList/Main.java
@@ -10,16 +10,15 @@ public class Main {
             }
         }
 
-        Node nodeA = new Node(6);
-        Node nodeB = new Node(3);
-        Node nodeC = new Node(4);
-        Node nodeD = new Node(2);
-        Node nodeE = new Node(1);
-
-        nodeA.next = nodeB;
-        nodeB.next = nodeC;
-        nodeC.next = nodeD;
-        nodeD.next = nodeE;
+//        Node nodeA = new Node(6);
+//        Node nodeB = new Node(3);
+//        Node nodeC = new Node(4);
+//        Node nodeD = new Node(2);
+//        Node nodeE = new Node(1);
+//
+//        nodeA.next = nodeB;
+//        nodeB.next = nodeC;
+//        nodeC.next = nodeD;
+//        nodeD.next = nodeE;
     }
 }
- 
