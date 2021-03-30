340cf1fbe37e2767f0f5b759d306f388def47006
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-30 23:41:14 +0900

    Dynamic Polymorphism has been implemented

--- src/Dynamic_Polymorphism/Main.java
+++ src/Dynamic_Polymorphism/Main.java
@@ -1,5 +1,7 @@
 package Dynamic_Polymorphism;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 //        Polymorphism = many shapes/forms
@@ -8,5 +10,10 @@ public class Main {
          Animal animal;
          Dog dog;
          Cat cat;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What animal do you want ?");
+        System.out.println("(1 = dog) or (2 = cat)");
+
+        int choice = scanner.nextInt();
     }
 }
