c16fde82de3619a4b431698c78ef67086f2ed6a1
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-31 16:22:42 +0900

    Exception Handling has been implemented

--- src/ExceptionHandling/Main.java
+++ src/ExceptionHandling/Main.java
@@ -1,11 +1,28 @@
 package ExceptionHandling;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
         // exceptions = an event that occurs during the execution of a program that,
         //              disrupts the normal flow of instructions
 
+        try {
+            Scanner scanner= new Scanner(System.in);
+            System.out.println("Enter a whole number: ");
+            int x = scanner.nextInt();
+
+            System.out.println("Enter the second number to divide by: ");
+            int y = scanner.nextInt();
+
+            int z = x/y;
+
+            System.out.println("The result is " + z);
+        }
+        catch (ArithmeticException arithmeticException){
+            System.out.println(" You can't divide a number by \"zero\"  ");
+        }
 
     }
 }
