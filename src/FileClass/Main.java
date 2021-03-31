95025d427907e73a402106d8906bcdc61364d317
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-31 17:12:47 +0900

    File Class has been implemented

--- src/FileClass/Main.java
+++ src/FileClass/Main.java
@@ -1,9 +1,18 @@
 package FileClass;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args){
         // file =  An abstract representation of a file and directory pathnames
 
 
+        File file = new File("message.txt");
+        if (file.exists()){
+            System.out.println("That file exists ! ");
+        }
+        else {
+            System.out.println("That file doesn't exist");
+        }
     }
 }
