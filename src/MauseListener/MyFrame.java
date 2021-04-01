49f4cd76ba993f1992150311f229fafe2e7c10ce
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-04-01 11:45:06 +0900

    MouseListener method has been implemented

---
+++ src/MauseListener/MyFrame.java
@@ -0,0 +1,41 @@
+package MauseListener;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MyFrame extends JFrame implements MouseListener {
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(null);
+        this.setVisible(true);
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
