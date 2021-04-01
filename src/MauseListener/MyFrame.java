a86a33fd402dc1bda4efb157aa8f4f4c9045f7f1
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-04-01 12:20:56 +0900

    MouseListener method has been implemented

--- src/MauseListener/MyFrame.java
+++ src/MauseListener/MyFrame.java
@@ -1,16 +1,27 @@
 package MauseListener;
-
+import java.awt.Color;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class MyFrame extends JFrame implements MouseListener {
+    JLabel jLabel;
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 500);
         this.setLayout(null);
+
+        JLabel label = new JLabel();
+        label.setBounds(0,0,100,100);
+        label.setBackground(Color.red);
+        label.setOpaque(true);
+        label.addMouseListener(this);
+
+        this.add(label);
         this.setVisible(true);
+
 
     }
 
