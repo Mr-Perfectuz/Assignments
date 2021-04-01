07d6466c9795d1110299ca64e1e3aca29a3c81bc
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-04-01 13:00:02 +0900

    Audio has been implemented

--- src/JavaAudio/Main.java
+++ src/JavaAudio/Main.java
@@ -1,14 +1,28 @@
 package JavaAudio;
 import javax.sound.sampled.*;
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
 
+        Scanner scanner = new Scanner(System.in);
         File file = new File("C:\\developer\\DSU\\src\\JavaAudio\\19th_Floor.wav");
 
-        if (file.exists()){
-            System.out.println("File exists");
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+
+
+
+        String response = scanner.next();
+        response = response.toUpperCase();
+
+        while (!response.equals("Q")){
+            System.out.println("P = play  \nS = stop \nR = reset  \nQ = quit ");
         }
+
+        clip.start();
     }
 }
